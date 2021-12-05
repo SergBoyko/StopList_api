e10a630a6c232bfa9c94854fb417a1997b4d51dc
Sergey Boyko <badwolf6661@icloud.com>
2021-12-05 22:10:15 +0300

    Add api for cheking phone number

--- app/controllers/api/v1/phone_controller.rb
+++ app/controllers/api/v1/phone_controller.rb
@@ -1,6 +1,17 @@
 module Api
   module V1
-    class PhonesController < ApplicationController
+    class PhoneController < ApplicationController
+      skip_before_action :verify_authenticity_token
+      def check
+        phone = params['phone']
+        resp = { found: '' }
+        resp[:found] = if Stoplist.exists?(phone: phone)
+                         1
+                       else
+                         0
+                       end
+        render json: resp, status: :ok
+      end
     end
   end
 end
